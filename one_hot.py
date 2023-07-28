16ceed7d378be73888cf2be2fdd7f6acebf8358f
Arquimedes <108104701+Archads@users.noreply.github.com>
2023-07-28 06:30:01 -0700

    profiling dados caegóricos pelo onehot

--- one_hot.py
+++ one_hot.py
@@ -4,13 +4,7 @@ df = pd.read_parquet('data/dataset_streamlit.parquet')
 
 colunas_categoricas = ['etnia', 'genero', 'fonte_admissao_uti', 'tipo_estadia_uti', 'tipo_uti', 'sistema_corporal_apache_3j', 'sistema_corporal_apache_2']
 
-df_encoded = pd.get_dummies(df, columns=colunas_categoricas)
+df_encoded = pd.get_dummies(df, columns=colunas_categoricas, drop_first=True)
 
-# Selecionar apenas as colunas categóricas criadas
-colunas_criadas = df_encoded.columns[df_encoded.columns.str.startswith(tuple(colunas_categoricas))]
-
-# Criar um novo DataFrame com as colunas categóricas criadas
-df_categoricas_criadas = df_encoded[colunas_criadas]
-
-# Salvar as colunas categóricas criadas em um arquivo CSV
-df_categoricas_criadas.to_csv('colunas_categoricas_criadas_trad.csv', index=False)
+# Salvar o DataFrame com one-hot encoding em um arquivo CSV
+df_encoded.to_csv('dataset_streamlit_com_onehot.csv', index=False)
