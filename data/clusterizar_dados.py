f576c10b613e8408878e7c357cae94454e37e7ea
LEONARDOJORGE98 <56683773+LEONARDOJORGE98@users.noreply.github.com>
2023-07-31 18:09:45 -0300

    Update clusterizar_dados.py

--- data/clusterizar_dados.py
+++ data/clusterizar_dados.py
@@ -16,6 +16,7 @@ df = df.drop(columns=colunas_deletar)
 df = df[['ventilado_apache','probabilidade_morte_na_uti_(apache_4a)', 'd1_frequencia_cardiaca_maxima','d1_frequencia_cardiaca_minima', 'h1_frequencia_respiratoria_maxima','d1_spO2_minimo','d1_temperatura_minima', 'morte_hospital']]
 df = df.dropna()
 
+#calculado a partir do arquivo determinando_n_clusters(elbow)
 n_clusters = 2
 
 data = clusterizar_dados(df, n_clusters)
