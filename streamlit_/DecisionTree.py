f9c24532b6f4e17db94e2d3428c77077b5bbb35c
LEONARDOJORGE98 <56683773+LEONARDOJORGE98@users.noreply.github.com>
2023-08-22 22:04:58 -0300

    Update DecisionTree.py

--- streamlit_/DecisionTree.py
+++ streamlit_/DecisionTree.py
@@ -1,22 +1,33 @@
 import streamlit as st
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from imblearn.under_sampling import RandomUnderSampler
+
+
 
 # Carregar o conjunto de dados
 @st.cache_data
 def load_data():
 
-    data = pd.read_csv("C:/Users/leogo/OneDrive/Área de Trabalho/PISI3_2022.2-main/data/decisionTree_dataset.csv")
-    colunas_deletar = ['id_ncontro', 'id_paciente', 'id_hospital','id_uti']
+    data = pd.read_parquet("C:/Users/leogo/OneDrive/Área de Trabalho/PISI3_2022.2-main/data/decisionTree_dataset.parquet")
+    colunas_deletar = ['id_ncontro','id_paciente','id_hospital','id_uti','altura','peso','cirurgia_eletiva','genero_M'
+                       ,'etnia_Asian','etnia_Caucasian','etnia_Hispanic','etnia_Native American','etnia_Other/Unknown'
+                       ,'tipo_uti_CSICU','tipo_uti_CTICU','tipo_uti_Cardiac ICU','tipo_uti_MICU','tipo_uti_Med-Surg ICU'
+                       ,'tipo_uti_Neuro ICU', 'tipo_uti_SICU','gcs_olhos_apache','gcs_motor_apache','gcs_incapaz_apache'
+                       ,'gcs_verbal_pache','arf_apache']
     data = data.drop(columns=colunas_deletar)
     return data
 
+
 # Interface
 def DecisionTree():
+
+
     st.title("Análise do Árvore de Decisão")
     st.sidebar.header("Configurações")
 
@@ -24,18 +35,24 @@ def DecisionTree():
     data = load_data()
 
     # Selecionar colunas para recursos e alvo
-    features = st.sidebar.multiselect("Selecione as colunas de recursos", data.columns)
+    features = st.sidebar.multiselect("Selecione as colunas de recursos", data.columns, "imc")
     target = "morte_hospital"
 
     # Dividir o conjunto de dados em treinamento e teste
     X_train, X_test, y_train, y_test = train_test_split(data[features], data[target], test_size=0.2, random_state=42)
+
+
+    # Aplicar undersampling nas classes majoritárias
+    undersampler = RandomUnderSampler(random_state=42)
+    X_resampled, y_resampled = undersampler.fit_resample(X_train, y_train)
+
 
     # Hiperparâmetros
     max_depth = st.sidebar.slider("Profundidade máxima da árvore", 1, 20, 3)
 
     # Treinar o modelo
     model = DecisionTreeClassifier(max_depth=max_depth)
-    model.fit(X_train, y_train)
+    model.fit(X_resampled, y_resampled)
 
     # Avaliar o modelo
     y_pred = model.predict(X_test)
