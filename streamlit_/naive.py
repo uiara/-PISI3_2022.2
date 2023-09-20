215df7532e08e35cce4957c6b417e137640db533
Arquimedes <108104701+Archads@users.noreply.github.com>
2023-09-20 20:41:04 -0300

    comparações boxplot NaiveXrandomF

--- streamlit_/naive.py
+++ streamlit_/naive.py
@@ -5,8 +5,18 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import classification_report, confusion_matrix
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix, precision_score, recall_score, f1_score
+import plotly.express as px
 
-st.title('Classificação com Naive Bayes')
+st.title('Classificação com Naive Bayes e Random Forest')
+
 @st.cache
 def load_data():
     data = pd.read_parquet("data/renomeado_mediana_smoteenn.parquet")
@@ -26,19 +36,47 @@ def train_naive_bayes():
     nb_model.fit(X_train, y_train)
     return nb_model
 
+@st.cache
+def train_random_forest():
+    rf_model = RandomForestClassifier(random_state=42)
+    rf_model.fit(X_train, y_train)
+    return rf_model
+
 nb_model = train_naive_bayes()
+rf_model = train_random_forest()
 
 y_pred = nb_model.predict(X_test)
-#f1
-st.subheader('Relatório de Classificação:')
-report = classification_report(y_test, y_pred)
-st.text(report)
+y_pred_rf = rf_model.predict(X_test)
 
-st.subheader('Matriz de Confusão:')
-cm = confusion_matrix(y_test, y_pred)
-fig, ax = plt.subplots()
-sns.heatmap(cm, annot=True, fmt="d", cmap="YlGnBu", cbar=False, square=True, ax=ax) 
-ax.set_xlabel('Previsto', fontsize=12)
-ax.set_ylabel('Real', fontsize=12)
-ax.set_title('Matriz de Confusão', fontsize=16)
-st.pyplot(fig)
+st.subheader('Relatório de Classificação (Naive Bayes):')
+report_nb = classification_report(y_test, y_pred)
+st.text(report_nb)
+
+st.subheader('Matriz de Confusão (Naive Bayes):')
+cm_nb = confusion_matrix(y_test, y_pred)
+fig_nb, ax_nb = plt.subplots()
+sns.heatmap(cm_nb, annot=True, fmt="d", cmap="YlGnBu", cbar=False, square=True, ax=ax_nb) 
+ax_nb.set_xlabel('Previsto', fontsize=12)
+ax_nb.set_ylabel('Real', fontsize=12)
+ax_nb.set_title('Matriz de Confusão (Naive Bayes)', fontsize=16)
+st.pyplot(fig_nb)
+
+#métricas de desempenho para o Random Forest
+precision_rf = precision_score(y_test, y_pred_rf)
+recall_rf = recall_score(y_test, y_pred_rf)
+f1_rf = f1_score(y_test, y_pred_rf)
+
+#métricas de desempenho para o Naive Bayes
+precision_nb = precision_score(y_test, y_pred)
+recall_nb = recall_score(y_test, y_pred)
+f1_nb = f1_score(y_test, y_pred)
+
+metrics_data = pd.DataFrame({
+    'Métrica': ['Precisão', 'Recall', 'F1-Score'],
+    'Naive Bayes': [precision_nb, recall_nb, f1_nb],
+    'Random Forest': [precision_rf, recall_rf, f1_rf]
+})
+
+st.subheader('Box Plots das Métricas de Desempenho')
+fig_metrics = px.box(metrics_data, x='Métrica', y=['Naive Bayes', 'Random Forest'], title='Distribuição das Métricas de Desempenho')
+st.plotly_chart(fig_metrics)
